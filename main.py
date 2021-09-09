b0ce5f0d98fd2ce8f06906ebb11a5cc2008e3f52
hunter-banks <hbanks@zagmail.gonzaga.edu>
2021-09-09 14:46:04 -0700

    Power

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-x = 4/12
-print(x)
-x = 4//12
-print(x)
-x = 4%12
-print(x)
-x = 7//4
-print(x)
-x = 9.0/4.0
-print(x)
-#x = 3/0 #Error Divide by 0
-#print(x)
-x = 3.0%1
-print(x)
-#x = 16%0 #Error 
-#print(x)
-x = 3%5
-print(x)
-x = 9%5
-print(x)
-x = 2*4**2
-print(x)
-x = 2**4**(2/4)
-print(x)
+import math
+
+voltage = float(input("Please enter the voltage: "))
+resistance = float(input("Please enter the resistance: "))
+
+power = math.pow(voltage,2) / resistance
+print("\nThe power is:", "{:.2f}".format(power))
