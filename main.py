39cb4a286b712df7c99a166292ed04539e2b2c2e
hunter-banks <hbanks@zagmail.gonzaga.edu>
2021-09-09 14:22:05 -0700

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello Worldz")
+x = 4/12
+print(x)
+x = 4//12
+print(x)
+x = 4%12
+print(x)
+x = 7//4
+print(x)
+x = 9.0/4.0
+print(x)
+#x = 3/0 #Error Divide by 0
+#print(x)
+x = 3.0%1
+print(x)
+#x = 16%0 #Error 
+#print(x)
+x = 3%5
+print(x)
+x = 9%5
+print(x)
+x = 2*4**2
+print(x)
+x = 2**4**(2/4)
+print(x)
